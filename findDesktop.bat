8b5003e92073f2f9fa71c7a8041e450416128f78
Charlotte <88061511+sunburnt1352@users.noreply.github.com>
2025-08-25 13:19:57 -0400

    Update findDesktop.bat

--- findDesktop.bat
+++ findDesktop.bat
@@ -20,8 +20,9 @@ echo__________________________________________________████████__
 echo____________________________________________████████████________________________________
 echo______________________________________████████████______________________________________
 
-
+::first set to local user
 SETLOCAL
+::then use powershell to get the folder path to desktop
 FOR /F "usebackq" %%f IN (`PowerShell -NoProfile -Command "Write-Host([Environment]::GetFolderPath('Desktop'))"`) DO (
   SET "DESKTOP_FOLDER=%%f"
   )
